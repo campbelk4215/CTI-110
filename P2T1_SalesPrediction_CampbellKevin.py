b5e3483f8b1887bc39e09365c0eb2ada0907522b
campbelk4215 <campbelk4215@student.faytechcc.edu>
2018-06-28 12:12:40 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_CampbellKevin.py
@@ -0,0 +1,17 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Kevin Campbell
+#6-14-18
+
+#Assigns the value to totalSales
+totalSales = .23
+
+#Asks for the value to annualProfit (user input)
+annualProfit = int(input("What was the Annual Profit? "))
+
+#Calculates the projected total sales
+totalProjected = totalSales * annualProfit
+
+#Prints out the output
+print("The Projected Annual Profit is $", totalProjected)
+
